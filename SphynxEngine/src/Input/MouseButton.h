26335475591e1f3ef0737277dfa4071ada1c3629
DFaouaz <danyfaouaz@gmail.com>
2024-02-22 20:47:39 +0100

    Keycodes and MouseButtons added

---
+++ SphynxEngine/src/Input/MouseButton.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Core/Core.h"
+
+// Based on SDL mouse buttons
+namespace Sphynx
+{
+	enum SPHYNX_API MouseButton
+	{
+		SPX_BUTTON_LEFT = 1,
+		SPX_BUTTON_MIDDLE = 2,
+		SPX_BUTTON_RIGHT = 3,
+		SPX_BUTTON_X1 = 4,
+		SPX_BUTTON_X2 = 5
+	};
+}
--- SphynxEngine/src/Sphynx.h
+++ SphynxEngine/src/Sphynx.h
@@ -3,6 +3,10 @@
 // Include all client headers
 #include "Core/Application.h"
 
-
 // Entry point
 #include "Core/EntryPoint.h"
+
+// Input
+#include "Input/Input.h"
+#include "Input/Keycode.h"
+#include "Input/MouseButton.h"
